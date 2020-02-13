35c4c7793c9fb4d101ca62e84ec710332ef6140e
Johnvict <johnvict20@gmail.com>
2020-02-13 11:52:07 +0100

    To cpanel

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
 navigator.getBattery().then( battery => {
 	const batteryLevel =  `${battery.level * 100}%`;
-	window.alert(`Welcome to Tech Drive. Your battery level is ${batteryLevel}`);
+	window.alert(`Welcome to Tech Drive. \nYour battery level is ${batteryLevel}`);
 });
